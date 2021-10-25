c5eb2aaa260e9136934f07eb921198b6117bff03
hopto-dot <66906618+hopto-dot@users.noreply.github.com>
2021-10-25 15:16:59 +0100

    Add project files.

---
+++ JP Grammar Helper/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace JP_Grammar_Helper
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.createDatabase = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // createDatabase
+            // 
+            this.createDatabase.Location = new System.Drawing.Point(17, 17);
+            this.createDatabase.Name = "createDatabase";
+            this.createDatabase.Size = new System.Drawing.Size(86, 72);
+            this.createDatabase.TabIndex = 0;
+            this.createDatabase.Text = "Create Grammar Database";
+            this.createDatabase.UseVisualStyleBackColor = true;
+            this.createDatabase.Click += new System.EventHandler(this.createDatabase_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.createDatabase);
+            this.Name = "Form1";
+            this.Text = "JP Grammar Helper";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button createDatabase;
+    }
+}
+
---
+++ JP Grammar Helper/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JP_Grammar_Helper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void createDatabase_Click(object sender, EventArgs e)
+        {
+            WebClient client = new WebClient();
+            String HTML = string.Empty;
+            HTML = client.DownloadString(new Uri ("https://nihongokyoshi-net.com/jlpt-grammars/"));
+            HTML = HTML.Replace("span id=\"N", "`");
+            String[] grammarLevels = HTML.Split("`");
+            List<String[]> grammar = new List<string[]> { };
+            for (int i = 1; i < 6; i++)
+            {
+                grammarLevels[i] = grammarLevels[i].Replace("</a>", "`");
+            }
+
+            for (int i = 1; i < 6; i++)
+            {
+                grammar.Add(grammarLevels[i].Split("`"));
+            }
+
+            Debug.WriteLine("Imported database!");
+        }
+    }
+}
